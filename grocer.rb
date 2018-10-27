806e0aa4cd8e78a10ae4b77d77953454aa0036ea
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-27 13:02:35 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,60 @@
+require "pry"
+=begin 
+consolidate_cart
+    adds a count of one to each item when there are no duplicates (FAILED - 1)
+    increments count when there are multiple items (FAILED - 2)
+=end 
+
 def consolidate_cart(cart)
-  # code here
+      a2 =[]
+      cart.each do |hash|
+        a2 << hash.keys
+      end
+      a3 = a2.flatten.inject(Hash.new(0)) do |collection, value|
+        collection[value] = collection[value] + 1
+        collection
+      end
+  
+      cart.each do |hash|
+        hash.each do |item, stats|
+            stats[:count] = a3[item]
+        end
+      end
+      
+  result_hash = {}
+    cart.each do |hash|
+      hash.each do |item, stats|
+        result_hash[item] = stats
+      end
+    end
+  
+result_hash
 end
 
+
+
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    coupon.each do |stat, value|
+      
+      if cart.keys.any? {|x| x == value}
+        cart[value][:count] = cart[value][:count]- coupon[:num]
+        
+        cart["#{value} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[value][:clearance], :count => 1}
+      end
+    end
+  end
+cart
 end
 
 def apply_clearance(cart)
-  # code here
+  
+  cart.each do |item, attributes|
+    if cart[item][:clearance] == true
+      cart[item][:price] *= 0.8
+      cart[item][:price] = cart[item][:price].round(2)
+    end
+  end
 end
 
 def checkout(cart, coupons)
